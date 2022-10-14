57991dee62ca3f23e91e5b2a928f6087f6acfbcd
Matej Lamza <matej.lamza@decode.agency>
2022-10-14 12:39:15 +0200

    WIP: Custom collapsing toolbar

---
+++ app/src/main/java/com/example/weatherlamza/ui/TestFragment.kt
@@ -0,0 +1,62 @@
+package com.example.weatherlamza.ui
+
+import android.os.Bundle
+import android.view.View
+import com.example.weatherlamza.common.base.BaseFragment
+import com.example.weatherlamza.databinding.FragmentWeatherCollapsingBinding
+import com.example.weatherlamza.ui.search.adapters.RecentSearchesAdapter
+import com.example.weatherlamza.utils.extensions.gone
+import com.example.weatherlamza.utils.extensions.visible
+import kotlin.math.abs
+
+class TestFragment :
+    BaseFragment<FragmentWeatherCollapsingBinding>(FragmentWeatherCollapsingBinding::inflate) {
+
+    private val recentSearchesAdapter by lazy { RecentSearchesAdapter() }
+    private val mockListSearches = listOf(
+        "bla",
+        "bla",
+        "ble",
+        "blo",
+        "blu",
+        "bli",
+        "bleh",
+        "hme",
+        "jesus",
+        "loves",
+        "you",
+        "do",
+        "you",
+        "love",
+        "him",
+        "and",
+        "why",
+        "not"
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUI()
+
+        binding.topBarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val isCollapsed = abs(verticalOffset) - appBarLayout!!.totalScrollRange == 0
+
+            if (isCollapsed) {
+                binding.toolbar.visible()
+            } else {
+                binding.toolbar.gone()
+            }
+        }
+    }
+
+
+    private fun setUI() {
+        with(binding) {
+            recentSearches.adapter = recentSearchesAdapter
+            recentSearchesAdapter.recentlySearchedQueries = mockListSearches
+        }
+    }
+
+}
+
+
